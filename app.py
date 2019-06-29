19fb86f8207d6c975e7f594a6d8b2dfc7677ddf3
naresh <rnaresh.n@gmail.com>
2019-06-29 11:22:38 -0400

    docs added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, jsonify, render_template, request, send_from_directory, url_for
 
 
+# Change jinja template syntax
 class CustomFlask(Flask):
     jinja_options = Flask.jinja_options.copy()
     jinja_options.update(dict(
@@ -9,13 +10,13 @@ class CustomFlask(Flask):
         variable_end_string='%%',
     ))
 
+
 # Create symbolic link to the required path, this is needed to download files in the appropriate format
 default_path: str = "/home/naresh/Downloads"
 symbolic_path = "./static/linked_dir"
 if os.path.exists(symbolic_path):
     os.remove(symbolic_path)
 os.symlink(default_path, symbolic_path)
-
 default_path = os.path.abspath(symbolic_path)
 
 app = CustomFlask(__name__)  # This replaces your existing "app = Flask(__name__)"
@@ -39,31 +40,39 @@ def list_files_n_dirs(path: str):
 
 @app.route("/")
 def home():
+    """Render home page
+    
+    Returns:
+        Rendered template
+    """
     return render_template("./index.html")
 
 
 @app.route("/default_dir")
 def default_dir():
+    """Returns the default home directory as a JSON object
+    
+    Returns:
+        JSON: default home directory
+    """
     return jsonify({"default_dir": default_path})
 
 
 @app.route("/ls")
 def ls():
+    """Returns a JSON object contatining files and folders from the desired path.
+    Path must be passsed as the request URL parameters with the key 'path'
+    
+    Returns:
+        JSON: {"files": an object containing all the files with numeric indices as keys
+               "dirs": an object containing all the directories with numeric indices as keys}
+    """
     dir_path = request.args.get("path")
     dir_files , dir_dirs = list_files_n_dirs(dir_path)
     response = {"files": {i: j for i, j in enumerate(dir_files)},
                 "dirs": {i: j for i,j in enumerate(dir_dirs)}}
     return jsonify(response) 
 
-
-@app.route("/download_file")
-def download_file():
-    directory_path = request.args.get("dir")
-    file_name = request.args.get("file")
-    try:
-        return send_from_directory(directory_path, file_name, as_attachment=True)
-    except Exception as e:
-        return str(e)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -2,14 +2,17 @@ var app = new Vue({
     el: "#app",
     data: {
         message: "Test!!!",
-        default_dir: {},
-        navigation_stack: [],
+        default_dir: "",
         current_dir: "",
         current_dirs: {},
         current_files: {},
+        navigation_stack: [],
         disable_back: true
     },
     methods: {
+        /**
+        * Gets and sets the default home directory
+        */
         initial_setup: function() {
             axios.get("/default_dir")
             .then(function (response) {
@@ -24,6 +27,10 @@ var app = new Vue({
                 app.list_dir(app.current_dir)
             })
         },
+        /**
+        * Retrives and sets current_dirs and current_files variables 
+        * @param {string} path path to retrive and list files/dirs from 
+        */
         list_dir: function(path) {
             return axios.get("/ls", {
                         params: {
@@ -39,10 +46,10 @@ var app = new Vue({
                    .catch(function (error) {
                         console.log(error)
                    })
-                   .then(function () {
-                        // always executed
-                   })
         },
+        /**
+         * Sets current_dir to the previous dir and updates navigation_stack
+         */
         navigate_back: function() {
             let previous_dir = app.navigation_stack.pop()
             app.current_dir = previous_dir
@@ -50,6 +57,10 @@ var app = new Vue({
             if (app.navigation_stack.length < 1)
                 app.disable_back = true
         },
+        /**
+         * Sets current_dir to the required dir and updates navigation_stack
+         * @param {string} dir directory to navigate to
+         */
         navigate_to: function(dir) {
             app.navigation_stack.push(app.current_dir)
             app.current_dir = app.current_dir + "/" + dir
@@ -57,10 +68,17 @@ var app = new Vue({
             if (app.navigation_stack.length > 0)
                 app.disable_back = false
         },
+        /**
+         * Returns link to the required file on the server
+         * @param {string} file file path relative to home/default directory
+         */
         get_file_link: function(file) {
             return "/static/linked_dir/" + file 
         }
     },
+    /**
+     * Performs initial setup when the page is loaded
+     */
     created: function() {
         this.initial_setup()
     }
